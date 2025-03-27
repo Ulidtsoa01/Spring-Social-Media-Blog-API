2a09a468598afe1116deeb560cc153b3e0cd6580
Daniel Huang <daniel.b.h.huang@gmail.com>
2025-03-27 08:26:49 +0000

    partial controller

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,12 +1,61 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 /**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ * TODO: You will need to write your own endpoints and handlers for your
+ * controller using Spring. The endpoints you will need can be
+ * found in readme.md as well as the test cases. You be required to use
+ * the @GET/POST/PUT/DELETE/etc Mapping annotations
+ * where applicable as well as the @ResponseBody and @PathVariable annotations.
+ * You should
+ * refer to prior mini-project labs and lecture materials for guidance on how a
+ * controller may be built.
  */
+@RestController
 public class SocialMediaController {
 
+  @Autowired
+  private AccountService accountService;
+
+  @Autowired
+  private MessageService messageService;
+
+  @ResponseStatus(value = HttpStatus.CONFLICT, reason = "Data integrity violation")
+  @PostMapping(value = "/register")
+  public ResponseEntity<Account> register(@RequestBody Account acct) throws Exception {
+    Account temp = accountService.registerUser(acct);
+    if (temp != null) {
+      return ResponseEntity.ok().body(temp);
+    }
+    return ResponseEntity.status(400).build();
+  }
+
+  @PostMapping(value = "/login")
+  public ResponseEntity<Account> login(@RequestBody Account account) {
+    Account accountLogged = accountService.login(account);
+    if (accountLogged != null) {
+      return ResponseEntity.ok().body(accountLogged);
+    } else {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+    }
+  }
+
+  @ExceptionHandler({ Exception.class })
+  public String handleException() {
+    return "Generic exception";
+  }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,6 +1,15 @@
 package com.example.service;
 
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.repository.AccountRepository;
 import com.example.entity.Account;
@@ -13,14 +22,14 @@ public class AccountService {
     this.acctRepo = acctRepo;
   }
 
-  public Account registerUser(Account acct) {
+  public Account registerUser(Account acct) throws Exception {
     if (acct.getUsername() == null | acct.getUsername().isEmpty() | acct.getPassword().length() < 4) {
       return null;
     }
-    if (acctRepo.findByUsername(acct.getUsername()) == null) {
-      return acctRepo.save(acct);
+    if (acctRepo.findByUsername(acct.getUsername()) != null) {
+      throw new Exception("dd");
     }
-    return null;
+    return acctRepo.save(acct);
   }
 
   public Account login(Account acct) {
